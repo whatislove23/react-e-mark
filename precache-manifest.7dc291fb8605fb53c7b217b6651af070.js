79048178f33dc2129103afd7043f2953c1aa4e0e
Vladyslav Tymchenko <vladyslavtymchenko@Vladyslavs-MacBook-Pro.local>
2022-09-07 14:43:49 +0300

    Updates

---
+++ precache-manifest.7dc291fb8605fb53c7b217b6651af070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc414aa31b3104f99bdf",
+    "url": "/react-e-mark/static/css/main.2385ab67.chunk.css"
+  },
+  {
+    "revision": "dc414aa31b3104f99bdf",
+    "url": "/react-e-mark/static/js/main.dc414aa3.chunk.js"
+  },
+  {
+    "revision": "7f953af55c79cfea82ac",
+    "url": "/react-e-mark/static/css/1.c9eb5291.chunk.css"
+  },
+  {
+    "revision": "7f953af55c79cfea82ac",
+    "url": "/react-e-mark/static/js/1.7f953af5.chunk.js"
+  },
+  {
+    "revision": "e4852da2a01b3d54b3e4",
+    "url": "/react-e-mark/static/js/runtime~main.e4852da2.js"
+  },
+  {
+    "revision": "df926413d4782334d38729bb177de31e",
+    "url": "/react-e-mark/index.html"
+  }
+];
